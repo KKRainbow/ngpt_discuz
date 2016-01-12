13873574177cc00d185bb220e16e4a78725f8fb9
孙思杰 <sunsijie@buaa.edu.cn>
2016-01-12 22:53:10 +0800

    环境变量配置

--- ngpt/config.inc.php
+++ ngpt/config.inc.php
@@ -6,9 +6,12 @@
  * Time: 上午6:51
  */
 
+$api_addr = getenv('SEED_API') ?: "http://127.0.0.1/seed/index.php?r=";
+$password = getenv('SEED_PASSWORD') ?: 'ngpt_2333';
+
 global $_G;
 $_G['ngpt'] = [
-    "api" => "http://127.0.0.1/seed/index.php?r=",
+    "api" => $api_addr,
     'dir' => DISCUZ_ROOT . "source/plugin/ngpt/",
-    'add_user_password' => 'ngpt_2333',
+    'add_user_password' => $password,
 ];
